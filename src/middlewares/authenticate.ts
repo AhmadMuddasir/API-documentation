0fe9d1fd4cf4b460ba5b0cfb311ab6636e861425
CodeReviser <ahmadmuddasir0786@gmail.com>
2026-01-20 10:51:06 +0530

    authentication page

--- src/middlewares/authenticate.ts
+++ src/middlewares/authenticate.ts
@@ -2,5 +2,12 @@ import type { NextFunction, Request, Response } from "express";
 
 const authenticate = (req:Request,res:Response,next:NextFunction)=>{
      //to get the token //token is always send in header
-     const token = 
+     const token = req.header('Authorization')
+          if(!token){
+               return next(createHttpError(401),"Authorization token is required")
+          }
+          //token parsing
+
+          const parsedToken 
+     
 }
